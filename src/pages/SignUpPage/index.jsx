4f97c72c95f6423958a751b8885b4f1663ea34ea
ananishimoto <ana.nishimoto@gmail.com>
2022-05-31 17:39:50 +0200

    Fixed sign up request and form page

--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -5,8 +5,30 @@ import AddCircleOutlineSharpIcon from "@mui/icons-material/AddCircleOutlineSharp
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectToken } from "../../store/user/selectors";
+import { signUp } from "../../store/user/actions";
 
 export default function SignUp() {
+  const [getName, setName] = useState("");
+  const [getEmail, setEmail] = useState("");
+  const [getPassword, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+
+  function submitForm(event) {
+    event.preventDefault();
+
+    dispatch(signUp(getName, getEmail, getPassword));
+
+    console.log("clean form:");
+
+    setName("");
+    setEmail("");
+    setPassword("");
+  }
+
   const signUpBackgroundStyle = {
     padding: 20,
     height: "70vh",
@@ -26,31 +48,39 @@ export default function SignUp() {
           <Typography>Already registered?</Typography>
           <NavLink to="/auth/login">Login</NavLink>
         </Grid>
-        <TextField
-          id="nameInput"
-          label="Name"
-          variant="standard"
-          fullWidth
-          required
-        />
-        <TextField
-          id="emailInput"
-          label="E-mail"
-          variant="standard"
-          fullWidth
-          required
-        />
-        <TextField
-          id="passwordInput"
-          label="Password"
-          variant="standard"
-          type="password"
-          fullWidth
-          required
-        />
-        <Button type="submit" color="secondary" variant="contained" fullWidth>
-          Sign Up
-        </Button>
+        <form onSubmit={submitForm}>
+          <TextField
+            id="nameInput"
+            label="Name"
+            variant="standard"
+            fullWidth
+            required
+            value={getName}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <TextField
+            id="emailInput"
+            label="E-mail"
+            variant="standard"
+            fullWidth
+            required
+            value={getEmail}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <TextField
+            id="passwordInput"
+            label="Password"
+            variant="standard"
+            type="password"
+            fullWidth
+            required
+            value={getPassword}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <Button type="submit" color="secondary" variant="contained" fullWidth>
+            Sign Up
+          </Button>
+        </form>
       </Paper>
     </Grid>
   );
--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,30 +1,32 @@
 import { apiUrl } from "../../config/constants";
 import axios from "axios";
 import { selectToken } from "./selectors";
-// import { appLoading, appDoneLoading, setMessage } from "../appState/slice";
-// import { showMessageWithTimeout } from "../appState/actions";
+import { appLoading, appDoneLoading, setMessage } from "../appState/slice";
+import { showMessageWithTimeout } from "../appState/actions";
 import { loginSuccess, logOut, tokenStillValid } from "./slice";
 
 export const signUp = (name, email, password) => {
   return async (dispatch, getState) => {
     dispatch(appLoading());
     try {
-      const response = await axios.post(`${apiUrl}/auth/signup`, {
+      const { data } = await axios.post(`${apiUrl}/auth/signup`, {
         name,
         email,
         password,
       });
 
+      const { token, user } = data;
+
       dispatch(
         loginSuccess({
-          token: response.data.token,
-          user: response.data.user,
-          space: response.data.space,
+          token,
+          user,
         })
       );
       dispatch(showMessageWithTimeout("success", true, "account created"));
       dispatch(appDoneLoading());
     } catch (error) {
+      console.log("This is the error", { error });
       if (error.response) {
         console.log(error.response.data.message);
         dispatch(
@@ -49,100 +51,100 @@ export const signUp = (name, email, password) => {
   };
 };
 
-export const login = (email, password) => {
-  return async (dispatch, getState) => {
-    dispatch(appLoading());
-    try {
-      const response = await axios.post(`${apiUrl}/auth/login`, {
-        email,
-        password,
-      });
-      console.log("This is from user thunk", response.data);
-      dispatch(
-        loginSuccess({
-          token: response.data.token,
-          user: response.data.user,
-          space: response.data.space,
-        })
-      );
-      dispatch(showMessageWithTimeout("success", false, "welcome back!", 1500));
-      dispatch(appDoneLoading());
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.data.message);
-        dispatch(
-          setMessage({
-            variant: "danger",
-            dismissable: true,
-            text: error.response.data.message,
-          })
-        );
-      } else {
-        console.log(error.message);
-        dispatch(
-          setMessage({
-            variant: "danger",
-            dismissable: true,
-            text: error.response.data.message,
-          })
-        );
-      }
-      dispatch(appDoneLoading());
-    }
-  };
-};
+// export const login = (email, password) => {
+//   return async (dispatch, getState) => {
+//     dispatch(appLoading());
+//     try {
+//       const response = await axios.post(`${apiUrl}/auth/login`, {
+//         email,
+//         password,
+//       });
+//       console.log("This is from user thunk", response.data);
+//       dispatch(
+//         loginSuccess({
+//           token: response.data.token,
+//           user: response.data.user,
+//           space: response.data.space,
+//         })
+//       );
+//       dispatch(showMessageWithTimeout("success", false, "welcome back!", 1500));
+//       dispatch(appDoneLoading());
+//     } catch (error) {
+//       if (error.response) {
+//         console.log(error.response.data.message);
+//         dispatch(
+//           setMessage({
+//             variant: "danger",
+//             dismissable: true,
+//             text: error.response.data.message,
+//           })
+//         );
+//       } else {
+//         console.log(error.message);
+//         dispatch(
+//           setMessage({
+//             variant: "danger",
+//             dismissable: true,
+//             text: error.response.data.message,
+//           })
+//         );
+//       }
+//       dispatch(appDoneLoading());
+//     }
+//   };
+// };
 
-export const getUserWithStoredToken = () => {
-  return async (dispatch, getState) => {
-    // get token from the state
-    const token = selectToken(getState());
+// export const getUserWithStoredToken = () => {
+//   return async (dispatch, getState) => {
+//     // get token from the state
+//     const token = selectToken(getState());
 
-    // if we have no token, stop
-    if (token === null) return;
+//     // if we have no token, stop
+//     if (token === null) return;
 
-    dispatch(appLoading());
-    try {
-      // if we do have a token,
-      // check wether it is still valid or if it is expired
-      const response = await axios.get(`${apiUrl}/auth/me`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
+//     dispatch(appLoading());
+//     try {
+//       // if we do have a token,
+//       // check wether it is still valid or if it is expired
+//       const response = await axios.get(`${apiUrl}/auth/me`, {
+//         headers: { Authorization: `Bearer ${token}` },
+//       });
 
-      // token is still valid
-      // console.log("me thunk", response.data);
-      dispatch(
-        tokenStillValid({
-          user: response.data.user,
-          space: response.data.space,
-        })
-      );
-      dispatch(appDoneLoading());
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.message);
-      } else {
-        console.log(error);
-      }
-      // if we get a 4xx or 5xx response,
-      // get rid of the token by logging out
-      dispatch(logOut());
-      dispatch(appDoneLoading());
-    }
-  };
-};
+//       // token is still valid
+//       // console.log("me thunk", response.data);
+//       dispatch(
+//         tokenStillValid({
+//           user: response.data.user,
+//           space: response.data.space,
+//         })
+//       );
+//       dispatch(appDoneLoading());
+//     } catch (error) {
+//       if (error.response) {
+//         console.log(error.response.message);
+//       } else {
+//         console.log(error);
+//       }
+//       // if we get a 4xx or 5xx response,
+//       // get rid of the token by logging out
+//       dispatch(logOut());
+//       dispatch(appDoneLoading());
+//     }
+//   };
+// };
 
-export function deleteSpecificStory(id) {
-  return async function thunk(dispatch, getState) {
-    try {
-      dispatch(appLoading());
-      const response = await axios.delete(`${apiUrl}/spaces/story/${id}`);
-      // console.log("This is the delete thunk", response.data);
+// export function deleteSpecificStory(id) {
+//   return async function thunk(dispatch, getState) {
+//     try {
+//       dispatch(appLoading());
+//       const response = await axios.delete(`${apiUrl}/spaces/story/${id}`);
+//       // console.log("This is the delete thunk", response.data);
 
-      dispatch(deleteStory(id));
-      // const detailSpace = response.data;
-      dispatch(appDoneLoading());
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-}
+//       dispatch(deleteStory(id));
+//       // const detailSpace = response.data;
+//       dispatch(appDoneLoading());
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   };
+// }
