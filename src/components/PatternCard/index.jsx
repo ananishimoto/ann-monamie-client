da763a1614b6e7ee6ecc454a0e932fcd582c6e53
ananishimoto <ana.nishimoto@gmail.com>
2022-05-31 13:56:51 +0200

    Added details page with hardcoded info

---
+++ src/components/PatternCard/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+export default function PatternCard() {
+  return (
+    <Grid container>
+      <Grid item>
+        <h1>Title</h1>
+        <img
+          src="https://i.pinimg.com/originals/3a/38/a7/3a38a7f50ae8fc3ed77ef2a52aaae902.jpg"
+          alt="whale"
+          width="500"
+        />
+      </Grid>
+      <Grid item>
+        <h2>Materials</h2>
+        <p>Some text here</p>
+        <h2>Pattern</h2>
+        <p>Some more text here</p>
+      </Grid>
+    </Grid>
+  );
+}
--- src/pages/DetailsPage/index.jsx
+++ src/pages/DetailsPage/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
+import { Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { Grid } from "@mui/material";
+import PatternCard from "../../components/PatternCard";
 
 export default function DetailsPage() {
   return (
-    <div>
+    <Grid>
       <NavBar />
-      <h1>This is the details page</h1>
-    </div>
+      <NavLink to="/projects">
+        <Button color="secondary" variant="contained">
+          Back to your projects
+        </Button>
+      </NavLink>
+      <Grid>
+        <PatternCard />
+      </Grid>
+    </Grid>
   );
 }
