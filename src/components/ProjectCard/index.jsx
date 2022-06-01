f1756f2bf02341ab587e6c05fd055d193564bc6d
ananishimoto <ana.nishimoto@gmail.com>
2022-06-02 00:35:59 +0200

    Added first style to your projects page

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,25 +1,40 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 export default function ProjectCard(props) {
+  const mainGrid = {
+    display: "flex",
+    flexWrap: "wrap",
+  };
+
+  const cardBackgroundStyle = {
+    padding: 20,
+    height: 100,
+    width: 400,
+    margin: "20px auto",
+  };
+
   console.log("Card", Object.keys(props.projects));
+
   return (
-    <div>
+    <Grid style={mainGrid}>
       {Object.keys(props.projects).map((status) => (
-        <div key={status}>
+        <Grid key={status}>
           <h2>{status}</h2>
           {props.projects[status].map((project) => (
             <Grid key={project.id}>
-              <Grid>
-                <NavLink to={`/project/${project.id}`}>
-                  <p>{project.name}</p>
-                </NavLink>
-              </Grid>
+              <Paper elevation={5} style={cardBackgroundStyle}>
+                <Grid>
+                  <NavLink to={`/project/${project.id}`}>
+                    <p>{project.name}</p>
+                  </NavLink>
+                </Grid>
+              </Paper>
             </Grid>
           ))}
-        </div>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 }
